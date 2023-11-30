a84b6aa6de9790f179334208ab7b94dca5c59b12
ACH90 <chassagne.alexandre@gmail.com>
2023-11-30 10:36:45 +0100

    Modif 2

--- GenerateurQuestions.cs
+++ GenerateurQuestions.cs
@@ -1,3 +1,5 @@
+
+
 public class GenerateurQuestions
 {
     public void GenererQuestionsDepuisCSV(string cheminFichierCSV)
@@ -10,24 +12,58 @@ public class GenerateurQuestions
             {
                 string[] colonnes = ligne.Split(';'); //Diviser chaque ligne en colonnes en utilisant le point-virgule comme délimiteur
 
-                if (colonnes.Length == 3) //Cette condition vérifie si chaque ligne a le bon format, c'est-à-dire si elle contient trois colonnes : question, réponses et réponse correcte
-                {
-                    string question = colonnes[0];                  //Extraction de la question à partir de la première colonne
-                    string reponses = colonnes[1];                  //Extraction des réponses à partir de la deuxième colonne
-                    int reponseCorrecte = int.Parse(colonnes[2]);   //Extraction de la réponse correcte à partir de la troisième colonne et conversion en entier
+                if (colonnes.Length == 4) //Cette condition vérifie si chaque ligne a le bon format, c'est-à-dire si elle contient quatre colonnes : catégorie, question, réponses et réponse correcte
+                {   
+                    string categorie = colonnes[0];                    //Extraction de la catégorie à partir de la premiere colone
+                    string question = colonnes[1];                  //Extraction de la question à partir de la deuxième colonne
+                    string reponses = colonnes[2];                  //Extraction des réponses à partir de la troisième colonne
+                    int reponseCorrecte = int.Parse(colonnes[3]);   //Extraction de la réponse correcte à partir de la quatrieme colonne et conversion en entier
 
                     string[] optionsReponses = reponses.Split('/'); //créer un nouveau tableau de chaines qui s'appelle optionReponses dans lequel on va y mettre les diférentes reponses proposées dans le csv qui sont séparées par /
 
-                    Console.WriteLine($"Question : {question}");        //Afficher la question
+                    Console.WriteLine($@"Catégorie : {categorie}" + "\n");      //Afficher la catégorie
+                    Console.WriteLine($"Question : {question}" + "\n");        //Afficher la question
 
-                    for (int i = 0; i < optionsReponses.Length; i++)    //Pour chaques questions afficher les différentes réponses
+                    //for (int i = 0; i < optionsReponses.Length; i++)    //Pour chaques questionsn, tant que reponse est 1,2 ou 3 afficher les différentes réponses
+                    //{
+                    //    Console.WriteLine($"{i + 1}. {optionsReponses[i]}");
+                    //}
+
+
+                    int i = 1; // Initialisation du compteur pour afficher les options
+                    foreach (var option in optionsReponses)
                     {
-                        Console.WriteLine($"{i + 1}. {optionsReponses[i]}");
+                 
+                        Console.WriteLine($"{i}. {option}"); // Afficher chaque option avec son numéro
+                       
+                        i++;
                     }
+                    int reponseUtilisateur = int.Parse(Console.ReadLine());          //Lecture de la réponse de l'utilisateur et conversion en entier
+                    if (reponseUtilisateur == 1 || reponseUtilisateur == 2 || reponseUtilisateur == 3)
+                    {
+                        if (reponseUtilisateur == reponseCorrecte)          //Si reponse utilisateur == reponse correcte afficher "Correct!"
+                        {
+                            Console.WriteLine("Correct !\n");
+                        }
+                        else                                               // Sinon afficher $"Incorrect.La réponse correcte est : { reponseCorrecte}\n"
+                        {
+                            Console.WriteLine($"Incorrect. La réponse correcte est : {reponseCorrecte}\n");
+                        }
+
+                    }
+                    else
+                    {
+                        // Le code à exécuter si la réponse utilisateur n'est pas égale à 1, 2 ou 3
+                        Console.WriteLine($@"La réponse utilisateur n'est pas 1, 2 ou 3." + "\n");
+                    }
+                    // Autres actions si nécessaire
+
 
                     Console.Write("Votre réponse : ");                  //Afficher "Votre réponse : "
 
-                    GestionReponse.CorrigerReponse(reponseCorrecte);
+
+
+                    //GestionReponse.CorrigerReponse(reponseCorrecte);
                 }
                 else
                 {
--- GestionReponse.cs
+++ GestionReponse.cs
@@ -1,16 +1,28 @@
 ﻿public class GestionReponse
 {
     public static void CorrigerReponse(int reponseCorrecte)
-    {
+    {   
         int reponseUtilisateur = int.Parse(Console.ReadLine());          //Lecture de la réponse de l'utilisateur et conversion en entier
 
-        if (reponseUtilisateur == reponseCorrecte)          //Si reponse utilisateur == reponse correcte afficher "Correct!"
+        if (reponseUtilisateur == 1 || reponseUtilisateur == 2 || reponseUtilisateur == 3)
         {
-            Console.WriteLine("Correct !\n");
+            if (reponseUtilisateur == reponseCorrecte)          //Si reponse utilisateur == reponse correcte afficher "Correct!"
+            {
+                Console.WriteLine("Correct !\n");
+            }
+            else                                               // Sinon afficher $"Incorrect.La réponse correcte est : { reponseCorrecte}\n"
+            {
+                Console.WriteLine($"Incorrect. La réponse correcte est : {reponseCorrecte}\n");
+            }
+
         }
-        else                                               // Sinon afficher $"Incorrect.La réponse correcte est : { reponseCorrecte}\n"
+        else
         {
-            Console.WriteLine($"Incorrect. La réponse correcte est : {reponseCorrecte}\n");
+            // Le code à exécuter si la réponse utilisateur n'est pas égale à 1, 2 ou 3
+            Console.WriteLine($@"La réponse utilisateur n'est pas 1, 2 ou 3." + "\n");
         }
+
+
+        
     }
 }
